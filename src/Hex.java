feb136fe048f52f42a7a95a4657b7f0c8075deff
Ian Murphy <iano200@gmail.com>
2013-09-28 09:28:08 +0100

    Initial commit for Hex project

---
+++ src/Hex.java
@@ -0,0 +1,129 @@
+
+public class Hex implements BoardGame {
+
+	private int[][] board; // 2D Board. 0 - empty, 1 - Player 1, 2 - Player 2 
+	                      
+	private int n1, n2; // height and width of board
+	
+	private WeightedQuickUnionUF wqu; // Union Find data structure to keep track
+										// of unions and calculate winner
+	
+	private int currentPlayer; // Current player in the game, initialised to 1
+
+	public Hex(int n1, int n2) // create N-by-N grid, with all sites blocked
+	{
+        this.n1 = n1;
+        this.n2 = n2;
+        currentPlayer = 1;
+
+        // TODO: Create instance of board
+        board = new int[n1][n2];
+        for (int i = 0; i < board.length; i++)
+            for (int j = 0; j < board.length; j++)
+                board[i][j] = 0;
+
+        // TODO: Create instance WeightedQuickUnionUF class
+        // (n1 * n2) + 4 because we need an extra 4 to check whose won
+        wqu = new WeightedQuickUnionUF((n1 * n2) + 4);
+
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see BoardGame#takeTurn(int, int)
+	 */
+	@Override
+	public void takeTurn(int x, int y) {
+
+		// TODO: check coords are valid
+
+		// TODO: check if location is free and set to player's value(1 or 2).
+
+		// TODO: calculate location and neighbours location in
+		// WeightedQuickUnionUF data structure
+
+		// TODO: create unions to neighbour sites in WeightedQuickUnionUF that
+		// also contain current players value
+
+		// TODO: if no winner get the next player
+
+	}
+
+	
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see BoardGame#getCurrentPlayer()
+	 */
+	@Override
+	public int getCurrentPlayer() {
+		return currentPlayer;
+	}
+
+	public void setCurrentPlayer(int currentPlayer) {
+		this.currentPlayer = currentPlayer;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see BoardGame#getBoard()
+	 */
+	@Override
+	public int[][] getBoard() {
+		return board;
+	}
+
+	private void nextPlayer() {
+		if (currentPlayer == 1)
+			currentPlayer = 2;
+		else
+			currentPlayer = 1;
+
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see BoardGame#isWinner()
+	 */
+	@Override
+	public boolean isWinner() {
+
+		// TODO:check if there is a connection between either side of the board.
+		// You can do this by using the 'virtual site' approach in the
+		// percolation test.
+		return false;
+	}
+	
+	/**
+	 * THIS IS OPTIONAL:
+	 * Modify the main method if you wish to suit your implementation.
+     * This is just an example of a test implementation.
+     * For example you may want to display the board after each turn.
+     * @param args
+	 * 
+	 */
+	public static void main(String[] args) {
+
+		BoardGame hexGame = new Hex(4, 4);
+
+		while (!hexGame.isWinner()) {
+			System.out.println("It's player " + hexGame.getCurrentPlayer()
+					+ "'s turn");
+			System.out.println("Enter x and y location:");
+			int x = StdIn.readInt();
+			int y = StdIn.readInt();
+
+			hexGame.takeTurn(x, y);
+
+		}
+
+		System.out.println("It's over. Player " + hexGame.getCurrentPlayer()
+				+ " wins!");
+
+	}
+
+}
---
+++ src/HexTest.java
@@ -0,0 +1,115 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+/**
+ * 
+ * This JUnit tests the Hex class.
+ * If you have coded it correctly they should all pass!
+ * This is not an exhaustive test - you can create your own test cases.
+ * @author Frank
+ *
+ */
+public class HexTest {
+
+	/**
+	 * This test checks the initial creation of a Hex class.
+	 */
+	@Test
+	public void HexInitialisationTest() {
+		BoardGame hex = new Hex(14,14);
+		//check board dimensions
+		assertTrue(hex.getBoard().length==14);
+		assertTrue(hex.getBoard()[0].length==14);
+		
+		//check board initialised to 0
+		assertTrue(hex.getBoard()[0][0]==0);
+		assertTrue(hex.getBoard()[0][0]==hex.getBoard()[13][13]);
+		
+		//check game starts with player 1
+		assertTrue(hex.getCurrentPlayer()==1);
+		
+		//check there is no winner yet!
+		assertFalse(hex.isWinner());
+		
+	}
+	
+	
+	/**
+	 * This test checks a player 2 win scenario
+	 * This is using the board as it is shown in the spec.
+	 * In this case, player 2 tries to connect the top to the bottom(Yellow edges in image in specification)
+	 */
+	@Test
+	public void HexGamePlayer2WinTest() {
+		BoardGame hex = new Hex(14,14);
+		
+		int j=0;
+		assertFalse(hex.isWinner());
+		
+		for (int i:hex.getBoard()[0]){
+			hex.takeTurn(0, j);
+			assertFalse(hex.isWinner());
+			hex.takeTurn(1, j);
+			j++;	
+		}
+		
+		assertTrue(hex.isWinner() && hex.getCurrentPlayer()==2);
+
+	}
+	
+	
+	/**
+	 * This test checks a player 1 win scenario.
+	 * This is using the board as it is shown in the spec.
+	 * In this case, player 1 tries to connect the left and right sides(Yellow edges in board in specification)
+	 */
+	@Test
+	public void HexGamePlayer1WinTest() {
+		BoardGame hex = new Hex(14,14);
+		
+		int j=0;
+		assertFalse(hex.isWinner());
+		
+		for (int i:hex.getBoard()[0]){
+			assertTrue(hex.getCurrentPlayer()==1);
+			hex.takeTurn(j, 0);
+			hex.takeTurn(j, 1);
+			j++;	
+		}
+		
+		assertTrue(hex.isWinner() && hex.getCurrentPlayer()==1);
+	
+		
+	}
+	
+	/**
+	 * This test checks that the current player is not
+	 * changed if an invalid move is attempted.
+	 */
+	@Test
+	public void HexGameInvalidMoveTest(){
+		BoardGame hex = new Hex(14,14);
+		assertTrue(hex.getCurrentPlayer()==1);
+		
+		
+		hex.takeTurn(0, 0);
+		assertTrue(hex.getCurrentPlayer()==2);
+		
+		
+		hex.takeTurn(0, 0);//Location already used, current player should remain the same(i.e. player 2)
+		assertTrue(hex.getCurrentPlayer()==2);
+		
+		hex.takeTurn(10, 10);
+		assertTrue(hex.getCurrentPlayer()==1);
+		
+		hex.takeTurn(14, 10);//invalid Coordinate
+		assertTrue(hex.getCurrentPlayer()==1);
+		
+		hex.takeTurn(1, -10);//invalid Coordinate
+		assertTrue(hex.getCurrentPlayer()==1);
+		
+	}
+
+}
