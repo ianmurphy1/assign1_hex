dc9c361264440d9a2ace4228beefa637c5856704
Ian Murphy <iano200@gmail.com>
2013-10-02 11:08:41 +0100

    Refactored Tests

--- src/HexTest.java
+++ src/HexTest.java
@@ -41,51 +41,29 @@ public class HexTest {
          * In this case, player 2 tries to connect the top to the bottom(Yellow edges in image in specification)
          */
         @Test
-        public void HexGamePlayer2WinTest() {
+        public void HexGamePlayer1WinTest() {
             BoardGame hex = new Hex(14,14);
 
             int j=0;
             assertFalse(hex.isWinner());
 
             for (int i:hex.getBoard()[0]){
-			hex.takeTurn(0, j);
-			//assertFalse(hex.isWinner());
+			hex.takeTurn(3, j);
+
 			hex.takeTurn(1, j);
 			j++;	
 		}
 		
-		assertTrue(hex.isWinner() && hex.getCurrentPlayer()==1);
+		assertTrue(hex.isWinner() && hex.getCurrentPlayer() == 1);
 
 	}
 
-    /**
-     * This test checks a player 2 win scenario
-     * This is using the board as it is shown in the spec.
-     * In this case, player 2 tries to connect the top to the bottom(Yellow edges in image in specification)
-     */
-    @Test
-    public void HexGamePlayer2WinTestTemp() {
-        BoardGame hex = new Hex(14,14);
 
-        int j=0;
-        assertFalse(hex.isWinner());
-
-        for (int i:hex.getBoard()[0]){
-            hex.takeTurn(j, 0);
-            assertFalse(hex.isWinner());
-            hex.takeTurn(j, 1);
-            j++;
-        }
-
-        assertTrue(hex.isWinner() && hex.getCurrentPlayer()==2);
-
-    }
-	
 	/**
 	 * This test checks a player 1 win scenario.
 	 * This is using the board as it is shown in the spec.
 	 * In this case, player 1 tries to connect the left and right sides(Yellow edges in board in specification)
-	 */
+	 *//*
 	@Test
 	public void HexGamePlayer1WinTest() {
 		BoardGame hex = new Hex(14,14);
@@ -96,13 +74,14 @@ public class HexTest {
 		for (int i:hex.getBoard()[0]){
 			assertTrue(hex.getCurrentPlayer()==1);
 			hex.takeTurn(j, 0);
+			assertFalse(hex.isWinner());
 			hex.takeTurn(j, 1);
 			j++;	
 		}
 		
 		assertTrue(hex.isWinner() && hex.getCurrentPlayer()==1);
 		
-	}
+	}*/
 	
 	/**
 	 * This test checks that the current player is not
